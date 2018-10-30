1134bc90adfb907d07bb4222c71fe8f6c8377393
alakovic1 <alakovic1@etf.unsa.ba>
2018-10-30 16:46:06 +0100

    3.tut

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -4,13 +4,50 @@ import java.util.*;
 
 public class Imenik {
 
-    private HashMap<String, TelefonskiBroj> mapa;
+    private HashMap<String, TelefonskiBroj> mapa=new HashMap<String, TelefonskiBroj>();
 
-    public void dodaj(String ime, TelefonskiBroj broj);
-    public String dajBroj(String ime);
-    public String dajIme(TelefonskiBroj broj);
-    public String naSlovo(char s);
-    Set<String> izGrada(FiksniBroj.Grad g);
-    Set<TelefonskiBroj> izGradaBrojevi(FiksniBroj.Grad g);
+    public void dodaj(String ime, TelefonskiBroj broj){
+        mapa.put(ime,broj);
+    }
 
+    public String dajBroj(String ime){
+        return mapa.get(ime).ispisi();
+    }
+
+    public String dajIme(TelefonskiBroj broj){
+        Set set=mapa.entrySet();
+        Iterator iterator=set.iterator();
+        while(iterator.hasNext()){
+            Map.Entry mentry=(Map.Entry)iterator.next();
+            if(mentry.getValue().equals(broj)){
+                return (String) mentry.getKey();
+            }
+        }
+        return "";
+    }
+
+    public String naSlovo(char s){
+        int vrijednost=1;
+        String ispis=new String();
+        for(Map.Entry<String,TelefonskiBroj> map : mapa.entrySet()) {
+            ispis += vrijednost + ". " + map.getKey() + " - " + map.getValue();
+            System.out.println(ispis);
+            vrijednost++;
+        }
+        return "";
+    }
+
+    Set<String> izGrada(FiksniBroj.Grad g){
+
+    }
+
+    Set<TelefonskiBroj> izGradaBrojevi(FiksniBroj.Grad g){
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+    
 }
--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,9 +1,19 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public abstract class TelefonskiBroj {
+public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj> {
 
     public abstract String ispisi();
 
     @Override
     public abstract int hashCode();
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int compareTo(TelefonskiBroj o) {
+        return 0;
+    }
 }
